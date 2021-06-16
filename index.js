d6088ba80e164ad9b04245bd56a970ae98b4d67e
jennychndr <57401522+jennychndr@users.noreply.github.com>
2021-06-16 16:10:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const mysql= require('mysql');
+
+
+const app= express();
+const routes= {
+    user: require('./routes/user'),
+    transaksi: require('./routes/transaksi')
+};
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",function(req,res){
+    return res.json("halo")
+})
+app.use('/api/users', routes.user); //route untuk pakai mysql raw
+ //route untuk pakai mysql + promise
+ app.use('/api/transaksi', routes.transaksi); 
+
+
+app.listen(3000, () => console.log('Running on port 3000'));
+
+
